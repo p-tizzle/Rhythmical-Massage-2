6849894beff618dab0a875681d55d4fff70acba7
p-tizzle <jerrytinney@icloud.com>
2016-03-08 14:07:13 -0600

    js update

--- index.html.0.js
+++ index.html.0.js
@@ -23,3 +23,27 @@
   $('.carousel').carousel({
    interval: 2800
   });
+  
+  
+  
+ /**************************************
+          GOOGLE MAP
+ **************************************/
+
+ function initMap() {
+        var myLatLng = {lat: 38.285612, lng: -85.627257}
+           
+        var mymap = new google.maps.Map(document.getElementById('mymap'), {
+          zoom: 16,
+          center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: mymap,
+          title: 'Massage/RMT'
+        });
+      }
+    
+    
+    
