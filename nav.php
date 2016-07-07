dd36455cf37fd7d515be144b8ec8945e64e518be
Jerry  Tinney <jerrytinney@dhcp013100.code-louisville-wifi.lmcdc.local>
2016-07-07 17:22:57 -0400

    include file

---
+++ nav.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+   
+          <title><?php echo "Sabrina Ford Massage" ?></title>
+                <link rel="stylesheet" href="normalize.css">
+                <link rel="stylesheet" href="main.css">
+                <link rel="stylesheet" href="responsive.css">
+                <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+                      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+                      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </head>
+          <body>
+                <div class="container-fluid">
+
+<div>
+                  <h1 class="title">Massage/RMT Therapy <br>Sabrina Ford</h1>
+            </div>
+            <div>
+                  <nav class="links">
+                        <ul id="centerButon">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="About.php">About Me</a></li>
+                            <li><a href="Sessions.php">Sessions</a></li>
+                            <li><a href="Hours.php">Hours</a></li>
+                            <li><a href="Contact.php">Contact</a></li>
+                        </ul>
+                  </nav>
+            </div>
+                  
+                
